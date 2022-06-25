b1d243a72ed56e7a1a8a4c906f028a7e259b706f
adrianmarino <adrianmarino@gmail.com>
2022-06-25 11:46:23 -0300

    feat: predict and rename datasest.csv columns

--- lib/plot.py
+++ lib/plot.py
@@ -7,20 +7,28 @@ SENTINEL_BANDS = { 'red': 3, 'green': 3, 'blue': 2 }
 
 
 def raster_visible_bands(raster, bands_by_color = SENTINEL_BANDS):
-    red   = raster.GetRasterBand(bands_by_color['red'])
-    green = raster.GetRasterBand(bands_by_color['green'])
-    blue  = raster.GetRasterBand(bands_by_color['blue'])
-    return [red, green, blue]
+    bands =  []
+    if 'red' in bands_by_color:
+        bands.append(raster.GetRasterBand(bands_by_color['red']))
+        
+    if 'green' in bands_by_color:
+        bands.append(raster.GetRasterBand(bands_by_color['green']))
+    
+    if 'blue' in bands_by_color:
+        bands.append(raster.GetRasterBand(bands_by_color['blue']))
+
+    return bands
 
 
-def plot_raw_raster(bands):
-    img = np.dstack([b.ReadAsArray() for b in bands])
-    plt.imshow(img)
+def plot_raw_raster(bands, figsize):
+    img = np.dstack([b.ReadAsArray() for b in bands])    
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.imshow(img, interpolation='nearest')
     plt.show()
     return img
 
 
-def plot_raster(path, bands_by_color = SENTINEL_BANDS):
+def plot_raster(path, bands_by_color = SENTINEL_BANDS, figsize=(15, 10)):
     raster = gdal.Open(path)
-    plot_raw_raster(raster_visible_bands(raster, bands_by_color))
+    plot_raw_raster(raster_visible_bands(raster, bands_by_color), figsize)
     plt.savefig(f'{path}.png')
